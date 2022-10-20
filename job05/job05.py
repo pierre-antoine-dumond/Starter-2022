3e7be61438773f130630c07681413d053a1da7a2
pierre-antoine-dumond <114387884+pierre-antoine-dumond@users.noreply.github.com>
2022-10-20 15:45:37 +0200

    Delete job05 directory

--- job05/job05.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Valeur 1 :"))
-y = int(input("Valeur 2 :"))
-for i in range(x+1, y, +1):
-        print(i)
-for i in range(x-1, y ,-1):
-        print(i)
-if x == y:
-    print("Valeurs égales")
